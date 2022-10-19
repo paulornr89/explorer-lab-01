2cb8006b3d03110195b0ab2ade17a2313ace9dc8
paulornr <paulornr89@gmail.com>
2022-10-19 10:29:51 -0300

    aula 1 concluida

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+console.log(ccLogo)
+setCardType("default")
+//console.log(ccBgColor01)
+function setCardType(typeCard) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[typeCard][0])
+  ccBgColor02.setAttribute("fill", colors[typeCard][1])
+  ccLogo.setAttribute("src", `/cc-${typeCard}.svg`)
+}
+
+globalThis.setCardType = setCardType
